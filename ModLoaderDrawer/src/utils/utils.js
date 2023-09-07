d9ad32eeeb6fb728398dfd21c09155a97a3cd2b4
CMoyuer <1531964875@qq.com>
2023-09-07 11:22:44 +0800

    changed mod name

--- ModLoaderDrawer/src/utils/utils.js
+++ ModLoaderDrawer/src/utils/utils.js
@@ -6,7 +6,7 @@ export function getLocale() {
 
 export function getModInfo(res, key) {
 	let language = getLocale()
-	if (language !== "en" && res.info[key + "_i18n"] && res.info[key + "_i18n"][language]) {
+	if (res.info[key + "_i18n"] && res.info[key + "_i18n"][language]) {
 		return res.info[key + "_i18n"][language]
 	} else {
 		return res.info[key]
